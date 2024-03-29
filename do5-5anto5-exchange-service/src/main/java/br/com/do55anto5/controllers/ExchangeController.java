e0649e24ecdd15b910d913cad7a952c975f88ed4
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-03-29 15:37:47 -0300

    Recover executing Instance Port

--- do5-5anto5-exchange-service/src/main/java/br/com/do55anto5/controllers/ExchangeController.java
+++ do5-5anto5-exchange-service/src/main/java/br/com/do55anto5/controllers/ExchangeController.java
@@ -2,6 +2,8 @@ package br.com.do55anto5.controllers;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import br.com.do55anto5.model.Exchange;
 @RestController
 @RequestMapping("exchange-service")
 public class ExchangeController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Exchange getExchange(
@@ -19,6 +24,8 @@ public class ExchangeController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to
 			) {
-		return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		
+		var port = environment.getProperty("local.server.port");
+		return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
